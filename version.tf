cf364eceb1f921c1a7eb4fed797f22c8a602e99f
Fabusafat <smartdataengineer2025@gmail.com>
2026-08-10 12:45:43 +0100

    Phase 2 finished, it includes provider, version, data and variablaes

---
+++ version.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.12.0, < 2.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>5.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~>3.0"
+    }
+  }
+}
